ba7ab7a5f5162a9f58a721d2d54d440507aeff84
Andika Ahmad Ramadhan <ASpendaka@gmail.com>
2020-01-22 14:23:25 +0700

    run java jar file in deployment server

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -24,3 +24,4 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 scp target/${NAME}-${VERSION}.jar jenkins@192.168.10.150:/home/jenkins/app.jar
+ssh jenkins@192.168.10.150 'java -jar app.jar'
